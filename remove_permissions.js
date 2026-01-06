cf2f4fa93100c07611c5e093d97cc9869989f07e
Fir3line <96981745+Fir3line@users.noreply.github.com>
2026-01-06 19:53:24 +0000

    Update remove_permissions.js

--- remove_permissions.js
+++ remove_permissions.js
@@ -1,22 +1,33 @@
-var permissionsToRemove = [ "com.google.android.gms.permission.AD_ID"]; 
+var fs = require("fs");
+var path = require("path");
 
-var fs = require('fs'); 
-var path = require('path'); 
-var rootdir = ""; 
-var manifestFile = path.join(rootdir, "platforms/android/app/src/main/AndroidManifest.xml"); 
+var rootdir = "";
+var manifestFile = path.join(rootdir, "platforms/android/app/src/main/AndroidManifest.xml");
 
-fs.readFile( manifestFile, "utf8", function( err, data ) 
-{ 
-    if (err) 
-        return console.log( err ); 
+fs.readFile(manifestFile, "utf8", function (err, data) {
+    if (err)
+        return console.log(err);
 
-    var result = data; 
-    for (var i=0; i<permissionsToRemove.length; i++) 
-        result = result.replace( "<uses-permission android:name=\"" + permissionsToRemove[i] + "\" />", "" ); 
+    // If already present, do nothing
+    if (data.indexOf('android:largeHeap="true"') !== -1) {
+        console.log('[manifest-hook] android:largeHeap already present.');
+        return;
+    }
 
-    fs.writeFile( manifestFile, result, "utf8", function( err ) 
-    { 
-        if (err) 
-            return console.log( err ); 
-    } ); 
-} );
+    // Inject android:largeHeap="true" into <application> opening tag
+    var result = data.replace(
+        "<application",
+        '<application android:largeHeap="true"'
+    );
+
+    if (result === data) {
+        console.log('[manifest-hook] <application> tag not found, nothing changed.');
+    } else {
+        console.log('[manifest-hook] Injected android:largeHeap="true".');
+    }
+
+    fs.writeFile(manifestFile, result, "utf8", function (err) {
+        if (err)
+            return console.log(err);
+    });
+});
